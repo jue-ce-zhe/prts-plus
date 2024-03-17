b38b4dff7f2b1cfcd28813f61b87e059e618ff46
jue-ce-zhe <162351003+jue-ce-zhe@users.noreply.github.com>
2024-03-17 17:38:40 -0400

    Add level name mappings

--- script/process_overview.py
+++ script/process_overview.py
@@ -5,5 +5,10 @@ with open('resource/map/overview.json', 'r', encoding='utf-8') as file:
 
 code_to_filename = {value['code']: value['filename'] for value in overview_data.values()}
 
-with open('resource/level_mapping.json', 'w', encoding='utf-8') as file:
+with open('resource/level_code_mapping.json', 'w', encoding='utf-8') as file:
     json.dump(code_to_filename, file, ensure_ascii=False, indent=4)
+
+name_to_filename = {value['name']: value['filename'] for value in overview_data.values()}
+
+with open('resource/level_name_mapping.json', 'w', encoding='utf-8') as file:
+    json.dump(name_to_filename, file, ensure_ascii=False, indent=4)
