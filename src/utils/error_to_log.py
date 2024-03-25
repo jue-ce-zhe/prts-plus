c1ebd109aeda5cbd4defac1942d33d25593d49cc
jue-ce-zhe <162351003+jue-ce-zhe@users.noreply.github.com>
2024-03-24 23:06:22 -0400

    Add __str__ method to ErrorToLog class

--- src/utils/error_to_log.py
+++ src/utils/error_to_log.py
@@ -1,4 +1,6 @@
 class ErrorToLog(Exception):
     def __init__(self, message: str, isError: bool = True):
         self.message = f"错误：{message}" if isError else f"{message}"
-    pass
+
+    def __str__(self):
+        return self.message
